d8b7afa24f6fff92c7b728680b0d0253c7372514
ashley.morris <ashley.morris@ultra-as.com>
2017-05-16 11:56:36 +0100

    moved files

---
+++ JavaScript/Main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ashley.morris on 15/05/2017.
+ */
+
+var func = require("./isDivisibleBy6");
+
+
+console.log(func.isDivisibleBy6("1234567890123456789012345678*0"));
---
+++ JavaScript/isDivisibleBy6.js
@@ -0,0 +1,27 @@
+/**
+ * Created by ashley.morris on 15/05/2017.
+ */
+
+function isDivisibleBy6(s) {
+    let num, res = [];
+
+    for (let i = 0; i < 10; i++)
+        if (isDivisibleBy(num = s.replace('*', i), 11)) {
+            res.push(num);
+        }
+
+    return res;
+}
+
+function isDivisibleBy(s, d) {
+    let remainder = 0;
+
+    for (let char of s) {
+        remainder = +(remainder + char) % d;
+    }
+
+
+    return !remainder;
+}
+
+module.exports.isDivisibleBy6 = isDivisibleBy6;
