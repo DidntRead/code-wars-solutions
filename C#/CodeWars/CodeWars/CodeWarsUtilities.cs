5e8d1643aa29f1450af2a08683a2803bdc540451
Ashley <ashley.morris@topcashback.co.uk>
2019-08-23 14:29:43 +0100

    added a new c# solution

---
+++ C#/CodeWars/CodeWars/CodeWarsUtilities.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWars
+{
+    class CodeWarsUtilities
+    {
+        public static bool GetOdd(int number)
+        {
+            if (number % 2 != 0)
+                return true;
+
+            return false;
+        }
+    }
+}
--- C#/CodeWars/CodeWars/Program.cs
+++ C#/CodeWars/CodeWars/Program.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CodeWars
 {
     class Program
     {
-        private Dictionary<string, int> _oddAndEvenCountDictionary = new Dictionary<string, int>();
-
         static void Main(string[] args)
         {
-            var x = Test("2 4 7 8 10");
+            int position = Test("2 4 7 8 10");
+            string alphabetPos = AlphabetPosition("The sunset sets at twelve o' clock.");
         }
 
 
@@ -22,23 +22,38 @@ namespace CodeWars
             int[] numbersAsArray = numbers.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
 
             var oddAndEvenCountDictionary = numbersAsArray
-                .GroupBy(GetOdd)
+                .GroupBy(CodeWarsUtilities.GetOdd)
                 .ToDictionary(group => group.Key.ToString().ToLower(), group => group.Count());
 
             oddAndEvenCountDictionary.TryGetValue("false", out var oddCount);
 
             return oddCount > 1
-                ? Array.FindIndex(numbersAsArray, GetOdd) + 1
-                : Array.FindIndex(numbersAsArray, x => !GetOdd(x)) + 1;
+                ? Array.FindIndex(numbersAsArray, CodeWarsUtilities.GetOdd) + 1
+                : Array.FindIndex(numbersAsArray, x => !CodeWarsUtilities.GetOdd(x)) + 1;
         }
 
 
-        static bool GetOdd(int number)
+        public static string AlphabetPosition(string text)
         {
-            if (number % 2 != 0)
-                return true;
+            text = Regex.Replace(text, "[^a-zA-Z0-9% ._]", string.Empty);
 
-            return false;
+            var processedText = new StringBuilder();
+            var alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".Split(',').ToArray();
+            var alphabetWithPos = new Dictionary<string, int>();
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabetWithPos[alphabet[i]] = i + 1;
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                alphabetWithPos.TryGetValue(text[i].ToString().ToLower(), out var value);
+                if (value != 0)
+                    processedText.Append($"{value} ");
+            }
+
+
+            return processedText.ToString();
         }
     }
 }
